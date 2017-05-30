e61c37b5086e370353b31966e9fc331f9b983cf2
Wassili Hense <hense.wassili@googlemail.com>
2017-05-30 15:44:22 +0200

    MQTT_SN CHG ScanPorts; ADD BlackList

--- plugins/MQTT_SN/MQTT_SNPl.cs
+++ plugins/MQTT_SN/MQTT_SNPl.cs
@@ -16,19 +16,14 @@ namespace X13.Periphery {
   [ExportMetadata("name", "MQTT_SN")]
   public class MQTT_SNPl : IPlugModul {
     private Topic _owner;
-    private bool _scanAllPorts;
-    private int _scanBusy;
     private Topic _verbose;
     private Random _rand;
     private SubRec _subMs;
 
-    internal AutoResetEvent _startScan;
     internal List<IMsGate> _gates;
     internal List<MsDevice> _devs;
 
     public MQTT_SNPl() {
-      _scanBusy = 0;
-      _startScan = new AutoResetEvent(false);
       _gates = new List<IMsGate>();
       _devs = new List<MsDevice>();
       _rand = new Random((int)DateTime.Now.Ticks);
@@ -117,176 +112,8 @@ namespace X13.Periphery {
         }
       } else if(p.art == Perform.Art.subAck) {
         _gates.Add(new MsGUdp(this));
-        ThreadPool.RegisterWaitForSingleObject(_startScan, ScanPorts, null, 45000, false);
-        _scanBusy = 1;
-        _startScan.Set();
+        MsGSerial.Init(this);
       }
-    }
-    private void ScanPorts(object o, bool b) {
-      if(Interlocked.CompareExchange(ref _scanBusy, 2, 1) != 1) {
-        return;
-      }
-
-      byte[] buf = new byte[64];
-      byte[] tmpBuf = new byte[64];
-      byte[] disconnectAll = new byte[] { 0x02, 0x02, 0x18, 0xC0 };
-      bool escChar;
-      int cnt = 0, tryCnt, length;
-      SerialPort port = null;
-      bool found;
-      DateTime to;
-
-      List<string> pns = new List<string>();
-      /*
-      Topic dev = Topic.root.Get("/dev");
-      lock(dev) {
-        var ifs = dev.children.OfType<DVar<MsDevice>>().Where(z => z.value != null).Select(z => z.value).ToArray();
-        foreach(var devSer in ifs) {
-          cnt++;
-          if(devSer.state == State.Connected) {
-            continue;
-          }
-          if(string.IsNullOrWhiteSpace(devSer.via)) {
-            _scanAllPorts = true;
-            break;
-          }
-          string via = devSer.via;
-          if(via != "offline" && !pns.Exists(z => string.Equals(z, via, StringComparison.InvariantCultureIgnoreCase))) {
-            pns.Add(via);
-          }
-        }
-      }*/
-      if(_scanAllPorts || cnt == 0) {
-        _scanAllPorts = false;
-        pns.Clear();
-        pns.AddRange(SerialPort.GetPortNames().Where(z=>!z.Contains("tty")));
-      } else {
-        pns = pns.Intersect(SerialPort.GetPortNames().Where(z => !z.Contains("tty"))).ToList();
-      }
-      /*
-      Topic tmp;
-      if(Topic.root.Exist("/local/cfg/MQTT-SN.Serial/whitelist", out tmp)) {
-        var whl = tmp as DVar<string>;
-        if(whl != null && !string.IsNullOrEmpty(whl.value)) {
-          var wps = whl.value.Split(';', ',');
-          if(wps != null && wps.Length > 0) {
-            pns = pns.Intersect(wps).ToList();
-          }
-        }
-      }
-      if(Topic.root.Exist("/local/cfg/MQTT-SN.Serial/blacklist", out tmp)) {
-        var bll = tmp as DVar<string>;
-        if(bll != null && !string.IsNullOrEmpty(bll.value)) {
-          var bps = bll.value.Split(';', ',');
-          if(bps != null && bps.Length > 0) {
-            pns = pns.Except(bps).ToList();
-          }
-        }
-      }
-      */
-      for(int i = 0; i < pns.Count; i++) {
-        if(_gates.Exists(z => z.name == pns[i])) {
-          continue;
-        }
-
-        try {
-          port = new SerialPort(pns[i], 38400, Parity.None, 8, StopBits.One);
-          port.ReadBufferSize = 300;
-          port.WriteBufferSize = 300;
-          port.ReadTimeout = 5;
-          port.Open();
-          tryCnt = 3;
-          found = false;
-          do {
-            to = DateTime.Now.AddMilliseconds(1100);
-            cnt = -1;
-            length = -1;
-
-            port.DiscardInBuffer();
-            port.Write(disconnectAll, 0, disconnectAll.Length);   // Send Disconnect
-            if(verbose) {
-              Log.Debug("s {0}: {1}  DISCONNECT", port.PortName, BitConverter.ToString(disconnectAll, 0, disconnectAll.Length));
-            }
-
-            while(to > DateTime.Now) {
-              if(port.BytesToRead > 0) {
-                to = DateTime.Now.AddMilliseconds(100);
-                if(cnt >= 0) {
-                  buf[cnt] = (byte)port.ReadByte();
-                } else {
-                  length = port.ReadByte();
-                }
-                cnt++;
-              }
-              Thread.Sleep(0);
-            }
-
-            if(cnt > 2 && cnt >= length) {
-              var msgTyp = (MsMessageType)(buf[0] > 1 ? buf[1] : buf[3]);
-              if(msgTyp == MsMessageType.SEARCHGW || msgTyp == MsMessageType.DHCP_REQ) {   // Received Ack
-                escChar = false;
-                if(cnt > length && buf[cnt - 1] == 0xC0) {
-                  int j, k = -1;
-                  for(j = 0; j < cnt; j++) {
-                    if(buf[j] == 0xDB) {
-                      escChar = true;
-                      continue;
-                    }
-                    if(escChar) {
-                      buf[++k] = (byte)(buf[j] ^ 0x20);
-                      escChar = false;
-                    } else {
-                      buf[++k] = buf[j];
-                    }
-                  }
-                  escChar = true;
-                  cnt = k;
-                }
-                if(cnt == length) {
-                  found = true;
-                  MsGSerial gw;
-                  lock(_gates) {
-                    gw = new MsGSerial(this, port);
-                    _gates.Add(gw);
-                  }
-                  gw._useSlip = escChar;
-                  if(verbose) {
-                    Log.Debug("I {0}: SLIP={1}", port.PortName, escChar);
-                  }
-                  ProcessInPacket(gw, gw._gateAddr, buf, 0, cnt);
-                  break;
-                }
-              }
-              if(verbose) {
-                Log.Debug("r {0}: {1}  {2}", pns[i], BitConverter.ToString(buf, 0, cnt), msgTyp);
-              }
-            }
-            Thread.Sleep(90);
-          } while(--tryCnt > 0);
-
-          if(!found) {
-            port.Close();
-            continue;
-          }
-        }
-        catch(Exception ex) {
-          if(verbose) {
-            Log.Debug("MQTT-SN.Serial search on {0} - {1}", pns[i], ex.Message);
-          }
-          try {
-            if(port != null) {
-              if(port != null && port.IsOpen) {
-                port.Close();
-              }
-              port.Dispose();
-            }
-          }
-          catch(Exception) {
-          }
-        }
-        port = null;
-      }
-      _scanBusy = 1;
     }
     internal bool ProcessInPacket(IMsGate gate, byte[] addr, byte[] buf, int start, int end) {
       var msg = MsMessage.Parse(buf, start, end);
--- plugins/MQTT_SN/MsGSerial.cs
+++ plugins/MQTT_SN/MsGSerial.cs
@@ -11,28 +11,104 @@ using X13.Repository;
 
 namespace X13.Periphery {
   internal class MsGSerial : IMsGate {
+    private static byte[] _disconnectAll;
+    private static int _scanBusy;
+    private static AutoResetEvent _startScan;
+    private static MQTT_SNPl _pl;
+
+    static MsGSerial() {
+      _scanBusy = 0;
+      _startScan = new AutoResetEvent(false);
+      _disconnectAll = new byte[] { 0x02, 0x02, 0x18, 0xC0 };
+    }
+
+    public static void Init(MQTT_SNPl pl) {
+      _pl = pl;
+      ThreadPool.RegisterWaitForSingleObject(_startScan, ScanSerialPorts, null, 45000, false);
+      _scanBusy = 1;
+      _startScan.Set();
+    }
+
+    private static void ScanSerialPorts(object o, bool b) {
+      if(Interlocked.CompareExchange(ref _scanBusy, 2, 1) != 1) {
+        return;
+      }
+      SerialPort port = null;
+      var serialT = Topic.root.Get("/$YS/MQTT-SN/serial");
+      var scanAllT = serialT.Get("ScanAll", true, serialT);
+      bool scanAllPorts = false;
+
+      if(scanAllT.GetState().ValueType != NiL.JS.Core.JSValueType.Boolean) {
+        scanAllT.SetAttribute(Topic.Attribute.DB);
+        scanAllT.SetState(false, serialT);
+      } else if((bool)scanAllT.GetState()) {
+        scanAllPorts = true;
+        scanAllT.SetState(false, serialT);
+      }
+
+      if(scanAllPorts || !_pl._devs.Any() || _pl._devs.Where(z => z.state == State.Lost || z.state == State.Disconnected).Select(z => z.owner.GetField("MQTT-SN.tag").Value as string).Any(z => z != null && z.Length > 2 && z[2] == 'S')) {
+        var pns = SerialPort.GetPortNames().Where(z => !z.Contains("tty")).ToArray();
+        for(int i = 0; i < pns.Length; i++) {
+          if(_pl._gates.Exists(z => z.name == pns[i])) {
+            continue;
+          }
+          {
+            string pn = pns[i];
+            int si = pn.LastIndexOf('/');
+            if(si>=0){
+              pn = pn.Substring(si+1);
+            }
+            var portT = serialT.Get(pn, true, serialT);
+            if(portT.GetState().ValueType != NiL.JS.Core.JSValueType.Boolean) {
+              portT.SetAttribute(Topic.Attribute.DB);
+              portT.SetState(true, serialT);
+            } else if((bool)portT.GetState() == false) {
+              continue;
+            }
+          }
+          try {
+            port = new SerialPort(pns[i], 38400, Parity.None, 8, StopBits.One);
+            port.ReadBufferSize = 300;
+            port.WriteBufferSize = 300;
+            port.ReadTimeout = 5;
+            port.Open();
+            new MsGSerial(port);
+          }
+          catch(Exception ex) {
+            if(_pl.verbose) {
+              Log.Debug("MQTT-SN.Serial search on {0} - {1}", pns[i], ex.Message);
+            }
+            try {
+              if(port != null) {
+                if(port != null && port.IsOpen) {
+                  port.Close();
+                }
+                port.Dispose();
+              }
+            }
+            catch(Exception) {
+            }
+          }
+          port = null;
+        }
+        foreach(var t in serialT.children.Where(z=>z.name != "ScanAll" && (z.GetState().ValueType != NiL.JS.Core.JSValueType.Boolean || (bool)z.GetState()) && pns.All(z1 => !z1.EndsWith(z.name)))) {
+          t.Remove(serialT);
+        }
+      }
+      _scanBusy = 1;
+    }
+
     private SerialPort _port;
     private Queue<MsMessage> _sendQueue;
-    private MQTT_SNPl _pl;
     private byte[] _sndBuf;
     private DateTime _advTick;
 
     internal bool _useSlip;
     internal byte[] _gateAddr;
 
-    public MsGSerial(MQTT_SNPl pl, SerialPort port) {
+    public MsGSerial(SerialPort port) {
       _port = port;
-      _pl = pl;
-      byte i = 1;
-      foreach(var g in _pl._gates) {
-        i = g.gwIdx >= i ? (byte)(g.gwIdx + 1) : i;
-      }
-      gwIdx = i;
-      int tmpAddr;
-      if(!int.TryParse(new string(_port.PortName.Where(z => char.IsDigit(z)).ToArray()), out tmpAddr) || tmpAddr == 0 || tmpAddr > 254) {
-        tmpAddr = (byte)(new Random()).Next(1, 254);
-      }
-      _gateAddr = new byte[] { gwIdx, (byte)tmpAddr };
+
       _sendQueue = new Queue<MsMessage>();
       _sndBuf = new byte[384];
 
@@ -56,10 +132,10 @@ namespace X13.Periphery {
       }
     }
     public void SendGw(MsDevice dev, MsMessage msg) {
-        msg.GetBytes();
-        lock(_sendQueue) {
-          _sendQueue.Enqueue(msg);
-        }
+      msg.GetBytes();
+      lock(_sendQueue) {
+        _sendQueue.Enqueue(msg);
+      }
     }
     public byte gwIdx { get; private set; }
     public byte gwRadius { get; private set; }
@@ -70,7 +146,7 @@ namespace X13.Periphery {
     public void Stop() {
       try {
         if(_port != null && _port.IsOpen) {
-          var nodes = _pl._devs.Where(z=>z._gate==this).ToArray();
+          var nodes = _pl._devs.Where(z => z._gate == this).ToArray();
           for(int i = 0; i < nodes.Length; i++) {
             nodes[i].Stop();
           }
@@ -188,6 +264,107 @@ namespace X13.Periphery {
       int len = -1;
       MsMessage msg;
       DateTime busyTime = DateTime.Now;
+      bool found = false;
+
+      try {
+        var tryCnt = 3;
+        do {
+          busyTime = DateTime.Now.AddMilliseconds(1100);
+          cnt = -1;
+          len = -1;
+
+          _port.DiscardInBuffer();
+          _port.Write(_disconnectAll, 0, _disconnectAll.Length);   // Send Disconnect
+          if(_pl.verbose) {
+            Log.Debug("s {0}: {1}  DISCONNECT", _port.PortName, BitConverter.ToString(_disconnectAll));
+          }
+
+          while(busyTime > DateTime.Now) {
+            if(_port.BytesToRead > 0) {
+              busyTime = DateTime.Now.AddMilliseconds(100);
+              if(cnt >= 0) {
+                buf[cnt] = (byte)_port.ReadByte();
+              } else {
+                len = _port.ReadByte();
+              }
+              cnt++;
+            }
+            Thread.Sleep(0);
+          }
+
+          if(cnt > 2 && cnt >= len) {
+            var msgTyp = (MsMessageType)(buf[0] > 1 ? buf[1] : buf[3]);
+            if(msgTyp == MsMessageType.SEARCHGW || msgTyp == MsMessageType.DHCP_REQ) {   // Received Ack
+              escChar = false;
+              if(cnt > len && buf[cnt - 1] == 0xC0) {
+                int j, k = -1;
+                for(j = 0; j < cnt; j++) {
+                  if(buf[j] == 0xDB) {
+                    escChar = true;
+                    continue;
+                  }
+                  if(escChar) {
+                    buf[++k] = (byte)(buf[j] ^ 0x20);
+                    escChar = false;
+                  } else {
+                    buf[++k] = buf[j];
+                  }
+                }
+                escChar = true;
+                cnt = k;
+              }
+              if(cnt == len) {
+                found = true;
+                _useSlip = escChar;
+                if(_pl.verbose) {
+                  Log.Debug("I {0}: SLIP={1}", _port.PortName, escChar);
+                }
+                _pl.ProcessInPacket(this, this._gateAddr, buf, 0, cnt);
+                break;
+              }
+            }
+            if(_pl.verbose) {
+              Log.Debug("r {0}: {1}  {2}", _port.PortName, BitConverter.ToString(buf, 0, cnt), msgTyp);
+            }
+          }
+          Thread.Sleep(90);
+        } while(--tryCnt > 0);
+      }
+      catch(Exception ex) {
+        if(_pl.verbose) {
+          Log.Debug("MQTT-SN.Serial search on {0} - {1}", _port.PortName, ex.Message);
+        }
+      }
+      if(!found) {
+        try {
+          if(_port != null) {
+            if(_port != null && _port.IsOpen) {
+              _port.Close();
+            }
+            _port.Dispose();
+          }
+        }
+        catch(Exception) {
+        }
+        return;
+      }
+
+      byte i = 1;
+      _pl._gates.Add(this);
+      foreach(var g in _pl._gates) {
+        i = g.gwIdx >= i ? (byte)(g.gwIdx + 1) : i;
+      }
+      gwIdx = i;
+      int tmpAddr;
+      if(!int.TryParse(new string(_port.PortName.Where(z => char.IsDigit(z)).ToArray()), out tmpAddr) || tmpAddr == 0 || tmpAddr > 254) {
+        tmpAddr = (byte)(new Random()).Next(1, 254);
+      }
+      _gateAddr = new byte[] { gwIdx, (byte)tmpAddr };
+
+      cnt = -1;
+      len = -1;
+      escChar = false;
+
       try {
         while(_port != null && _port.IsOpen) {
           if(GetPacket(ref len, buf, ref cnt, ref escChar)) {
@@ -216,14 +393,6 @@ namespace X13.Periphery {
             busyTime = DateTime.Now.AddMilliseconds(msg.IsRequest ? 20 : 5);
             continue;
           }
-          //if(_gwTopic != null && _gwTopic.value != null && (_gwTopic.value.state == State.Disconnected || _gwTopic.value.state == State.Lost)) {
-          //  _gwTopic = null;
-          //  Thread.Sleep(500);
-          //  this.Dispose();
-          //  Thread.Sleep(1000);
-          //  _pl._startScan.Set();
-          //  return;
-          //}
           if(_advTick < DateTime.Now) {
             SendRaw(new MsAdvertise(gwIdx, 900), _sndBuf);
             _advTick = DateTime.Now.AddMinutes(15);
@@ -233,7 +402,7 @@ namespace X13.Periphery {
       }
       catch(IOException ex) {
         if(_pl.verbose) {
-          Log.Error("MsGSerial({0}).CommThread() - {1}", gwIdx, ex.ToString());
+          Log.Error("MsGSerial({0}).CommThread() - {1}", gwIdx, ex.Message);
         }
       }
       catch(Exception ex) {
@@ -252,7 +421,6 @@ namespace X13.Periphery {
             p.Close();
           }
         }
-
         catch(Exception) {
         }
       }
