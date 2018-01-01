6bfaebcd3da26d53714013ef5c5c5102345f2146
Wassili Hense <hense.wassili@googlemail.com>
2018-01-01 21:15:37 +0100

    MQTT_SN CHG St, Sr -  String; base.xst ADD TWI/CC2D

--- plugins/MQTT_SN/MsDevice.cs
+++ plugins/MQTT_SN/MsDevice.cs
@@ -1245,8 +1245,8 @@ namespace X13.Periphery {
 
       new Tuple<string, DType>("Ms", DType.String),
 
-      new Tuple<string, DType>("St", DType.ByteArray),  // Serial port transmit
-      new Tuple<string, DType>("Sr", DType.ByteArray),  // Serial port recieve
+      new Tuple<string, DType>("St", DType.String),  // Serial port transmit
+      new Tuple<string, DType>("Sr", DType.String),  // Serial port recieve
       new Tuple<string, DType>("Ma", DType.ByteArray),  // Merkers
 
       new Tuple<string, DType>("pa", DType.ByteArray | DType.PLC),    // Program
