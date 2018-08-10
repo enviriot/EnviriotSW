955155d81d82f5f2c8d4f7894f54ef091f4b68dc
Wassili Hense <hense.wassili@googlemail.com>
2018-08-10 14:59:58 +0200

    ADD WebUI plugin

---
+++ plugins/WebUI/ApiV04.cs
@@ -0,0 +1,197 @@
+﻿///<remarks>This file is part of the <see cref="https://github.com/enviriot">Enviriot</see> project.<remarks>
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using WebSocketSharp;
+using WebSocketSharp.Net;
+using WebSocketSharp.Server;
+using X13.Repository;
+
+namespace X13.WebUI {
+  internal class ApiV04 : WebSocketBehavior {
+    private static Timer _pingTimer;
+    private static WebSocketSessionManager _wsMan;
+
+    static ApiV04() {
+      _pingTimer=new Timer(PingF, null, 270000, 300000);
+    }
+
+    private static void PingF(object o) {
+      if(_wsMan!=null) {
+        _wsMan.Broadping();
+      }
+    }
+
+    private List<X13.Repository.SubRec> _subscriptions;
+    private Session _ses;
+
+    protected override void OnOpen() {
+      if(_wsMan==null) {
+        _wsMan=Sessions;
+      }
+      string sid=null;
+      if(Context.CookieCollection["sessionId"]!=null) {
+        sid=Context.CookieCollection["sessionId"].Value;
+      }
+      System.Net.IPEndPoint remoteEndPoint = Context.UserEndPoint;
+      {
+        System.Net.IPAddress remIP;
+        if(Context.Headers.Contains("X-Real-IP") && System.Net.IPAddress.TryParse(Context.Headers["X-Real-IP"], out remIP)) {
+          remoteEndPoint=new System.Net.IPEndPoint(remIP, remoteEndPoint.Port);
+        }
+      }
+      _ses=Session.Get(sid, remoteEndPoint);
+      _subscriptions=new List<Repository.SubRec>();
+      Send(string.Concat("I\t", _ses.id, "\t", (string.IsNullOrEmpty(_ses.userName)?(/*_disAnonym.value?"false":*/"null"):"true")));
+      if(WebUI_Pl.verbose) {
+        X13.Log.Debug("{0} connect webSocket", _ses.owner.name);
+      }
+    }
+    protected override void OnMessage(MessageEventArgs e) {
+      string[] sa;
+      if(e.IsText && !string.IsNullOrEmpty(e.Data) && (sa=e.Data.Split('\t'))!=null && sa.Length>0) {
+        if(WebUI_Pl.verbose) {
+          X13.Log.Debug("ws.msg({0})", string.Join(", ", sa));
+        }
+        if(sa[0]=="C" && sa.Length==3) {  // Connect, username, password
+          /*if((sa[1]!="local" || _ses.ip.IsLocal()) && MQTT.MqBroker.CheckAuth(sa[1], sa[2])) {
+            _ses.userName=sa[1];
+            Send("C\ttrue");
+            if(WebUI_Pl.verbose) {
+              X13.Log.Info("{0} logon as {1} success", _ses.owner.name, _ses.ToString());
+            }
+          } else */{
+            Send("C\tfalse");
+            if(WebUI_Pl.verbose) {
+              X13.Log.Warning("{0}@{2} logon  as {1} failed", _ses.owner.name, sa[1], _ses.owner.GetState());
+            }
+            Sessions.CloseSession(base.ID);
+          }
+        } else if(/*!_disAnonym.value || */(_ses!=null /*&& !string.IsNullOrEmpty(_ses.userName)*/)) {
+          if(sa[0]=="P" && sa.Length==3) {
+            if(sa[1]!=null && sa[1].StartsWith("/export/")) {
+              WebUI_Pl.ProcessPublish(sa[1], sa[2], _ses);
+            } else {
+              X13.Log.Warning("{0}.publish({1}) - bad path",   (_ses==null || _ses.owner==null)?"UNK":_ses.owner.name, sa[1]);
+            }
+          } else if(sa[0]=="S" && sa.Length==2) {
+            if(sa[1]!=null && sa[1].StartsWith("/export/")) {
+              string p = sa[1];
+              SubRec.SubMask mask =  Repository.SubRec.SubMask.Value;
+              Topic t;
+              int idx = p.IndexOfAny(new[] { '+', '#' });
+              if(idx<0) {
+                mask |= SubRec.SubMask.Once;
+              } else if(idx == p.Length-1 && p[idx-1] == '/') {
+                mask |= p[idx] == '#'?SubRec.SubMask.All:SubRec.SubMask.Chldren;
+                p = p.Substring(0, p.Length-2);
+              } else {
+                X13.Log.Warning("{0}.subscribe({1}) - bad path", (_ses==null || _ses.owner==null)?"UNK":_ses.owner.name, sa[1]);
+                return;
+              }
+              if(Topic.root.Exist(p, out t)) {
+                _subscriptions.Add(t.Subscribe(mask, SubChanged));
+              } else {
+                X13.Log.Warning("{0}.subscribe({1}) - path not exist", (_ses==null || _ses.owner==null)?"UNK":_ses.owner.name, sa[1]);
+              }
+            } else {
+              X13.Log.Warning("{0}.subscribe({1}) - bad path", (_ses==null || _ses.owner==null)?"UNK":_ses.owner.name, sa[1]);
+            }
+          }
+        }
+      }
+    }
+
+    private void SubChanged(Perform p, SubRec sr) {
+      var vj = JsLib.Stringify(p.src.GetState());
+        Send(string.Concat("P\t", p.src.path, "\t", vj));
+        if(WebUI_Pl.verbose) {
+          X13.Log.Debug("ws.snd({0}, {1})", p.src.path, vj);
+        }
+    }
+    protected override void OnClose(CloseEventArgs e) {
+      if(_ses!=null) {
+        _ses.Close();
+        if(WebUI_Pl.verbose) {
+          X13.Log.Info("{0} Disconnect: [{1}]{2}", (_ses==null || _ses.owner==null)?"UNK":_ses.owner.name, e.Code, e.Reason);
+        }
+        _ses=null;
+      }
+      foreach(var s in _subscriptions) {
+        s.Dispose();
+      }
+    }
+  }
+  internal class Session : IDisposable {
+    private static List<WeakReference> sessions;
+
+    static Session() {
+      sessions=new List<WeakReference>();
+    }
+    public static Session Get(string sid, System.Net.IPEndPoint ep, bool create=true) {
+      Session s;
+      if(string.IsNullOrEmpty(sid) || (s=sessions.Where(z => z.IsAlive).Select(z => z.Target as Session).FirstOrDefault(z => z!=null && z.id==sid && z.ip.Equals(ep.Address)))==null) {
+        if(create) {
+          s=new Session(ep);
+          sessions.Add(new WeakReference(s));
+        } else {
+          s=null;
+        }
+      }
+      return s;
+    }
+
+    private Session(System.Net.IPEndPoint ep) {
+      Topic r=Topic.root.Get("/$YS/WebUI/clients");
+      this.id = Guid.NewGuid().ToString();
+      this.ip = ep.Address;
+      int i=1;
+      string pre=ip.ToString();
+      while(r.Exist(pre+i.ToString())) {
+        i++;
+      }
+      _owner=r.Get(pre+i.ToString());
+      owner.ClearAttribute(Topic.Attribute.Saved);
+      try {
+        var he=System.Net.Dns.GetHostEntry(this.ip);
+        _host=string.Format("{0}[{1}]", he.HostName, this.ip.ToString());
+        var tmp=he.HostName.Split('.');
+        if(tmp!=null && tmp.Length>0 && !string.IsNullOrEmpty(tmp[0])) {
+          i=1;
+          while(r.Exist(tmp[0]+"-"+i.ToString())) {
+            i++;
+          }
+          _owner.Move(r, tmp[0]+"-"+i.ToString());
+        }
+      }
+      catch(Exception) {
+        _host=string.Format("[{0}]", this.ip.ToString());
+      }
+      this.owner.SetState(_host);
+      if(WebUI_Pl.verbose) {
+        Log.Info("{0} session[{2}] - {1}", owner.name, this._host, this.id);
+      }
+    }
+    private string _host;
+    private Topic _owner;
+    public readonly string id;
+    public readonly System.Net.IPAddress ip;
+    public string userName;
+    public Topic owner { get { return _owner; } }
+    public void Close() {
+      sessions.RemoveAll(z => !z.IsAlive || z.Target==this);
+      Dispose();
+    }
+    public override string ToString() {
+      return (string.IsNullOrEmpty(userName)?"anonymus":userName)+"@"+_host;
+    }
+    public void Dispose() {
+      var o=Interlocked.Exchange(ref _owner, null);
+      if(o!=null && !o.disposed) {
+        o.Remove();
+      }
+    }
+  }
+}
---
+++ plugins/WebUI/WebUI_Pl.cs
@@ -0,0 +1,199 @@
+﻿///<remarks>This file is part of the <see cref="https://github.com/enviriot">Enviriot</see> project.<remarks>
+using JSC = NiL.JS.Core;
+using JSL = NiL.JS.BaseLibrary;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using X13.Repository;
+using System.Threading;
+using WebSocketSharp;
+using WebSocketSharp.Net;
+using WebSocketSharp.Server;
+using System.IO;
+
+namespace X13.WebUI {
+  [Export(typeof(IPlugModul))]
+  [ExportMetadata("priority", 9)]
+  [ExportMetadata("name", "WebUI")]
+
+  public class WebUI_Pl : IPlugModul {
+    internal static int ProcessPublish(string path, string json, Session ses) {
+      Topic cur=Topic.root.Get(path, true, ses==null?null:ses.owner);
+      if(string.IsNullOrEmpty(json) || json=="null") {                      // Remove
+        cur.Remove();
+        return 200;
+      }
+      cur.SetState(JsLib.ParseJson(json), ses==null?null:ses.owner);
+      return 200;
+    }
+
+    private Topic _owner;
+    private static Topic _verbose;
+    private HttpServer _sv;
+
+    #region IPlugModul Members
+    public void Init() {
+    }
+
+    public void Start() {
+      _owner = Topic.root.Get("/$YS/WebUI");
+      _verbose = _owner.Get("verbose");
+      if(_verbose.GetState().ValueType != JSC.JSValueType.Boolean) {
+        _verbose.SetAttribute(Topic.Attribute.Required | Topic.Attribute.DB);
+#if DEBUG
+        _verbose.SetState(true);
+#else
+        _verbose.SetState(false);
+#endif
+      }
+      var port = _owner.Get("port");
+      int port_i;
+      if(!port.GetState().IsNumber || (port_i = (int)port.GetState().Value)<=0 || port_i>65535) {
+        port_i = 8080;
+        port.SetState(port_i);
+      }
+      _sv = new HttpServer(port_i);
+      _sv.Log.Output=WsLog;
+#if DEBUG
+      _sv.Log.Level=WebSocketSharp.LogLevel.Trace;
+#endif
+      _sv.RootPath=Path.GetFullPath(Path.GetFullPath("../www"));
+      if(!Directory.Exists(_sv.RootPath)) {
+        Directory.CreateDirectory(_sv.RootPath);
+      }
+      _sv.OnGet+=OnGet;
+      _sv.AddWebSocketService<ApiV04>("/api/v04", () => new ApiV04());
+      _sv.Start();
+      if(_sv.IsListening) {
+        Log.Info("HttpServer started on {0}:{1} ", Environment.MachineName, _sv.Port.ToString());
+      } else {
+        Log.Error("HttpServer start failed");
+      }
+
+    }
+
+    public void Tick() {
+    }
+
+    public void Stop() {
+      _sv.Stop();
+    }
+
+    public bool enabled {
+      get {
+        var en = Topic.root.Get("/$YS/WebUI", true);
+        if(en.GetState().ValueType != JSC.JSValueType.Boolean) {
+          en.SetAttribute(Topic.Attribute.Required | Topic.Attribute.Readonly | Topic.Attribute.Config);
+          en.SetState(true);
+          return true;
+        }
+        return (bool)en.GetState();
+      }
+      set {
+        var en = Topic.root.Get("/$YS/WebUI", true);
+        en.SetState(value);
+      }
+    }
+    #endregion IPlugModul Members
+
+    public static bool verbose {
+      get {
+        return _verbose != null && (bool)_verbose.GetState();
+      }
+    }
+
+    private void WsLog(LogData d, string f) {
+      if(verbose) {
+        Log.Debug("WS({0}) - {1}", d.Level, d.Message);
+      }
+    }
+    private void OnGet(object sender, HttpRequestEventArgs e) {
+      var req = e.Request;
+      var res = e.Response;
+      if(req.RemoteEndPoint==null) {
+        res.StatusCode=(int)HttpStatusCode.NotAcceptable;
+        return;
+      }
+      System.Net.IPEndPoint remoteEndPoint = req.RemoteEndPoint;
+      {
+        System.Net.IPAddress remIP;
+        if(req.Headers.Contains("X-Real-IP") && System.Net.IPAddress.TryParse(req.Headers["X-Real-IP"], out remIP)) {
+          remoteEndPoint=new System.Net.IPEndPoint(remIP, remoteEndPoint.Port);
+        }
+      }
+      string path=req.RawUrl=="/"?"/index.html":req.RawUrl;
+      string client = null;
+      Session ses;
+      if(req.Cookies["sessionId"]!=null) {
+        ses=Session.Get(req.Cookies["sessionId"].Value, remoteEndPoint, false);
+      } else {
+        ses=null;
+      }
+
+      if(ses!=null && ses.owner!=null) {
+        client=ses.owner.name;
+      } else {
+        client=remoteEndPoint.Address.ToString();
+      }
+
+      try {
+        HttpStatusCode statusCode;
+        FileInfo f = new FileInfo(Path.Combine(_sv.RootPath, path.Substring(1)));
+        if(f.Exists) {
+          string eTag=f.LastWriteTimeUtc.Ticks.ToString("X8")+"-"+f.Length.ToString("X4");
+          string et;
+          if(req.Headers.Contains("If-None-Match") && (et=req.Headers["If-None-Match"])==eTag) {
+            res.Headers.Add("ETag", eTag);
+            statusCode=HttpStatusCode.NotModified;
+            res.StatusCode=(int)statusCode;
+            res.WriteContent(Encoding.UTF8.GetBytes("Not Modified"));
+          } else {
+            res.Headers.Add("ETag", eTag);
+            res.ContentType=Ext2ContentType(f.Extension);
+            using(var fs=f.OpenRead()) {
+              fs.CopyTo(res.OutputStream);
+              res.ContentLength64 = fs.Length;
+            }
+            statusCode=HttpStatusCode.OK;
+          }
+        } else {
+          statusCode=HttpStatusCode.NotFound;
+          res.StatusCode = (int)statusCode;
+          res.WriteContent(Encoding.UTF8.GetBytes("404 Not found"));
+        }
+
+        if(verbose) {
+          Log.Debug("{0} [{1}]{2} - {3}", client, req.HttpMethod, req.RawUrl, statusCode.ToString());
+        }
+      }
+      catch(Exception ex) {
+        if(verbose) {
+          Log.Debug("{0} [{1}]{2} - {3}", client, req.HttpMethod, req.RawUrl, ex.Message);
+        }
+      }
+    }
+    private string Ext2ContentType(string ext) {
+      switch(ext) {
+      case ".jpg":
+      case ".jpeg":
+        return "image/jpeg";
+      case ".png":
+        return "image/png";
+      case ".css":
+        return "text/css";
+      case ".csv":
+        return "text/csv";
+      case ".htm":
+      case ".html":
+        return "text/html";
+      case ".js":
+        return "application/javascript";
+      }
+      return "application/octet-stream";
+    }
+
+  }
+}
