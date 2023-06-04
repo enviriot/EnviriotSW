3e932ccce27329d6bf6b86800d3d8aa257cf1409
Wassili Hense <hense.wassili@googlemail.com>
2023-06-04 11:04:08 +0200

    WebUI(ADD) components/Text.js

--- Output/www/components/Button.js
+++ Output/www/components/Button.js
@@ -3,11 +3,10 @@
 class X13_button extends BaseComponent {
   init$ = {
     value: false,
-    text: "button",
     onUp: () => { this.$.value = false; wsBond.publish(this.dataset.value, this.$.value); },
     onDown: () => { this.$.value = true; wsBond.publish(this.dataset.value, this.$.value); }
   }
 }
-X13_button.template = /*html*/ '<button set="onmousedown:onDown;onmouseup:onUp">{{text}}</button>';
+X13_button.template = /*html*/ '<button set="onmousedown:onDown;onmouseup:onUp"><slot></slot></button>';
 
 X13_button.reg("x13-button");
---
+++ Output/www/components/Text.js
@@ -0,0 +1,19 @@
+﻿import { BaseComponent } from '/lib/symbiote.js';
+
+class X13_text extends BaseComponent {
+  init$ = {
+    value: false,
+    text: ""
+  }
+}
+X13_text.template = /*html*/ `
+  <style>
+  div { 
+    display: inline
+  }
+  </style>
+  <div> {{text}}</div >
+  `;
+X13_text.bindAttributes({"text":"text"});
+
+X13_text.reg("x13-text");
