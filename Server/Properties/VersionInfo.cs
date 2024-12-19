aaac2b2db3aff15bcb8eac873d4963474af5d5de
hensew <wassili.hensew@balluffstm.de>
2024-12-19 14:55:15 +0100

    PersistentStorageFDB(1st) Persistent Storage with Archive used Firebird embedded

--- Server/Properties/VersionInfo.cs
+++ Server/Properties/VersionInfo.cs
@@ -17,5 +17,5 @@ using System.Reflection;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("0.4.2311.29001")]
-[assembly: AssemblyFileVersion("0.4.2311.29001")]
+[assembly: AssemblyVersion("0.4.2412.19021")]
+[assembly: AssemblyFileVersion("0.4.2412.19021")]
