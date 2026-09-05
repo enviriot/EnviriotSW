dfc55df66012bfb22100c5a361826d7d82774cdb
wassili Hense <wassili@hense.email>
2026-09-05 16:41:25 +0200

    (CHG) VersionUpdate.exe -> VersionUpdate.targets

--- Server/Properties/VersionInfo.cs
+++ Server/Properties/VersionInfo.cs
@@ -17,5 +17,5 @@ using System.Reflection;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("0.5.2609.5002")]
-[assembly: AssemblyFileVersion("0.5.2609.5002")]
+[assembly: AssemblyVersion("0.5.2609.5102")]
+[assembly: AssemblyFileVersion("0.5.2609.5102")]
